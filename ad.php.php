6de75e6d04e0919ed893a3933076c266e5576473
anand2309 <63545231+anand2309@users.noreply.github.com>
2020-04-12 18:35:59 +0530

    Add files via upload

---
+++ ad.php.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<form action="question.php" method="POST">
+Question:<textarea name="ques"></textarea><br />
+A:<input type="text" name="op1" /><br />
+B:<input type="text" name="op2" /><br />
+C:<input type="text" name="op3" /><br />
+D:<input type="text" name="op4" /><br />
+Answer:<input type="text" name="ans" /><br />
+<input type="submit" name="submit" value="submit"/>
+</form>
+
+
+</body>
+</html>
